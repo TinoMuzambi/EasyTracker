cb3923cb95d00980811520677be07aa7fed5ce67
Tino Muzambi <tinomuzambi@gmail.com>
2022-06-04 00:39:52 +0200

    Adds integration with env variables.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from easy_equities_client.clients import EasyEquitiesClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 client = EasyEquitiesClient()
-client.login(username='your username', password='your password')
+client.login(username=os.environ.get("EASY_USERNAME"), password=os.environ.get("USER_PASSWORD"))
