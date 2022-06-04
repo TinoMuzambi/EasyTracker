6e744a1081debe657be19199cbe218ef76862139
Tino Muzambi <tinomuzambi@gmail.com>
2022-06-04 10:12:20 +0200

    Adds flask to requirements.

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+def home():
+    return render_template("index.html")\
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
