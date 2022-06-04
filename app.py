72f1866e2b03daf8d9b9a7a9c417048ffa367730
Tino Muzambi <tinomuzambi@gmail.com>
2022-06-04 10:17:00 +0200

    Adds year.

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from datetime import date
+
 
 app = Flask(__name__)
+
+@app.context_processor
+def inject_year():
+    return dict(year=date.today().year)
 
 @app.route('/')
 def home():
