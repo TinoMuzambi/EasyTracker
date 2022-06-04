f4832f3f6b134c5b0c9de1f2ceb9ad05fe7d5d95
Tino Muzambi <tinomuzambi@gmail.com>
2022-06-04 09:18:05 +0200

    Get holdings and accounts.

--- main.py
+++ main.py
@@ -8,4 +8,18 @@ client = EasyEquitiesClient()
 client.login(username=str(os.environ.get("EASY_USERNAME")), password=str(os.environ.get("EASY_PASSWORD")))
 
 accounts = client.accounts.list()
-print(accounts)
+accounts_empty_removed = []
+for account in accounts:
+    if account.name.find("Demo") == -1 and account.name.find("USD") == -1 and account.name.find("AUD") == -1 and account.name.find("Properties") == -1:
+        accounts_empty_removed.append(account)
+
+print("--------MY ACCOUNTS---------")
+for account in accounts_empty_removed:
+    print(account)
+
+print("--------MY HOLDINGS---------")
+for account in accounts_empty_removed:
+    print(account.name)
+    holdings = client.accounts.holdings(account.id)
+    for holding in holdings:
+        print(holding)
